e626d5fa5be8dde8a969117417ae2b89d77bb685
Julia Guenther <juliag1@alumni.stanford.edu>
2016-04-24 09:14:19 -0700

    Get_types working for negative inputs

--- csv.py
+++ csv.py
@@ -9,7 +9,7 @@ class csv(object):
 
 
     def parse_csv(self,filename):
-
+        
         csv_file = open(filename)
 
         closed_quotes = True
@@ -38,6 +38,7 @@ class csv(object):
         print closed_quotes
 
         print self.result_csv
+
 
     def combine_double_quotes(self,line_list, closed_quotes, quote_stack):
         new_list = []
@@ -73,18 +74,24 @@ class csv(object):
 
         This method assumes that there are only String and Numeric types in the file
 
+        It also assumes that the types of the columns are consistent throughout the file 
+        (i.e. if the types of the first row are ["String", "String", "Numeric"], the types in
+        each row will follow the pattern ["String", "String", "Numeric"])
+
         Specifically, String and Numeric types are defined as follows:
 
         String: any value of type str, including empty colums denoted by the placeholder " " 
 
-        Numeric: any value that contains only digits 
+        Numeric: any value that contains only digits or digits plus a negative sign (-) in
+        the first index or a decimal point in any index except the last index 
         """
 
         for item in self.result_csv[0]:
-            if item.isdigit():
+            if item[1:].isdigit() and (item[0] == '-' or item[0] == '.' or item[0].isdigit()):
                 self.type_list.append("Numeric")
             else:
                 self.type_list.append("String")
 
         return self.type_list
+    
 
